36561e23511100fe6a4a1be2ef653a70b4f3866a
brunorech <brunogrech@hotmail.com>
2019-12-02 19:18:28 -0300

    FEATURE: technologies table

--- src/database/index.js
+++ src/database/index.js
@@ -2,13 +2,17 @@ const Sequelize = require('sequelize');
 const config = require('../config/database');
 const User = require('../models/User');
 const Address = require('../models/Address');
+const Tech = require('../models/Tech');
 
 const connection = new Sequelize(config);
+const { models } = connection;
 
 User.init(connection);
 Address.init(connection);
+Tech.init(connection);
 
-User.associate(connection.models);
-Address.associate(connection.models);
+User.associate(models);
+Address.associate(models);
+Tech.associate(models);
 
 module.exports = connection;
