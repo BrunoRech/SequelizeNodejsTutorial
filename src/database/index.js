c14e68968bc30578237c5d51d8a67719f6983f27
brunorech <brunogrech@hotmail.com>
2019-12-02 16:37:21 -0300

    FEATURE: User model

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,9 @@
 const Sequelize = require(Sequelize);
 const config = require('../config/database');
+const User = require('../models/User');
 
 const connection = new Sequelize(config);
+
+User.init(connection);
 
 module.exports = connection;
